d460e5a234dccabe02deee667bd2bcdf5d789439
Zhu Junhe <2195150288@qq.com>
2024-09-17 14:04:23 +0800

    修改china地图，把省份后缀去掉，减少标题的长度

--- src/assets/map/provinces.js
+++ src/assets/map/provinces.js
@@ -1,14 +1,16 @@
 // src/utils/map/provinces.js
 
-import { guangdong } from './provinces/广东省';
-import { beijing } from './provinces/北京市';
-import { guangxi } from './provinces/广西壮族自治区';
+import { guangdong } from './provinces/广东';
+import { beijing } from './provinces/北京';
+import { guangxi } from './provinces/广西';
+import { shanghai } from './provinces/上海';
 // 继续导入其他省份
 
 // 使用中文全名作为键名
 export const provinces = {
-  '广东省': guangdong,
-  '北京市': beijing,
-  '广西壮族自治区': guangxi,
+  '广东': guangdong,
+  '北京': beijing,
+  '广西': guangxi,
+  '上海': shanghai,
   // 其他省份...
 };
